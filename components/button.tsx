ba95f17073f50420549252daf2e395d345d428cb
Sam Selikoff <sam.selikoff@gmail.com>
2023-02-08 17:00:47 -0500

    wip

--- components/button.tsx
+++ components/button.tsx
@@ -2,28 +2,16 @@ import { motion, useAnimationControls } from "framer-motion";
 import { forwardRef, ReactNode, useRef } from "react";
 import { FocusRing, useButton } from "react-aria";
 
-export default function Page() {
-  return (
-    <div className="m-10 border p-10">
-      <Button>Click</Button>
-    </div>
-  );
-}
-
-export const Button = forwardRef(function Button(
+export default forwardRef(function Button(
   {
-    // onClick = () => {},
     children,
     ...rest
   }: {
-    // onClick?: () => void;
     children: ReactNode;
   },
   ref
 ) {
-  // let ref = useRef(null);
   let controls = useAnimationControls();
-  console.log(ref);
 
   let { buttonProps } = useButton(
     {
@@ -47,38 +35,9 @@ export const Button = forwardRef(function Button(
     },
     ref
   );
-  // return <button ref={ref}>hi</button>;
-
-  // return (
-  //   <button
-  //     // {...buttonProps}
-  //     {...rest}
-  //     {...buttonProps}
-  //     ref={ref}
-  //     // animate={controls}
-  //     className="h-20 w-20 touch-none select-none rounded-full bg-[#353336] text-white focus:outline-none"
-  //     style={{
-  //       WebkitTapHighlightColor: "transparent",
-  //     }}
-  //   >
-  //     {children}
-  //   </button>
-  // );
 
   return (
     <FocusRing focusRingClass="ring ring-offset-2 ring-offset-black">
-      {/* <button
-        {...buttonProps}
-        {...rest}
-        ref={ref}
-        // animate={controls}
-        className="h-20 w-20 touch-none select-none rounded-full bg-[#353336] text-white focus:outline-none"
-        style={{
-          WebkitTapHighlightColor: "transparent",
-        }}
-      >
-        {children}
-      </button> */}
       <motion.button
         {...rest}
         {...buttonProps}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,97 @@
+import Button from "@/components/button";
 import * as Popover from "@radix-ui/react-popover";
 import { AnimatePresence, motion } from "framer-motion";
-import { forwardRef, ReactNode, useEffect, useRef, useState } from "react";
-import { Button } from "./button";
-// import { Button } from "./button";
+import { useRef, useState } from "react";
+
+function useRestartSetTimeout() {
+  let ref = useRef<ReturnType<typeof setTimeout>>();
+
+  return (timer: ReturnType<typeof setTimeout>) => {
+    clearTimeout(ref.current);
+    ref.current = timer;
+  };
+}
 
 export default function Index() {
   let [open, setOpen] = useState(false);
+  let restart = useRestartSetTimeout();
+
+  function handleOpenChange() {
+    setOpen(true);
+
+    restart(setTimeout(() => setOpen(false), 2000));
+  }
+
+  // let ref = useRef<ReturnType<typeof setTimeout>>();
+
+  // function handleOpenChange() {
+  //   setOpen(true);
+
+  //   clearTimeout(ref.current);
+
+  //   let timer = setTimeout(() => {
+  //     console.log("once");
+
+  //     setOpen(false);
+  //   }, 1000);
+
+  //   ref.current = timer;
+  // }
 
   return (
-    <div className="m-8 border p-8">
-      <Popover.Root open={open} onOpenChange={setOpen}>
-        <Popover.Trigger
-          asChild
-          className="hover:text-gray-400 focus:outline-none"
-        >
-          {/* <ClipboardIcon className="W-4 h-4" /> */}
-          {/* <LocalButton>Click me</LocalButton> */}
-          <Button>Click me</Button>
-          {/* <button>hi</button> */}
-        </Popover.Trigger>
+    <div className="m-20">
+      <div className="relative">
+        <div className="rounded bg-white px-4 py-3 font-mono text-sm shadow">
+          npm install react-aria framer-motion
+        </div>
 
-        <AnimatePresence>
-          {open && (
-            <Popover.Portal forceMount>
-              <Popover.Content
-                className=""
-                sideOffset={5}
-                side="top"
-                asChild
-                // align="center"
-              >
-                <motion.div
-                  initial="hidden"
-                  animate="showing"
-                  exit="hidden"
-                  transition={{ duration: 0.2 }}
-                  variants={{
-                    hidden: {
-                      y: 3,
-                      opacity: 0,
-                    },
-                    showing: {
-                      y: 0,
-                      opacity: 1,
-                    },
-                  }}
-                  className="focus:outline-none"
-                >
-                  <div className="rounded border border-white/20 bg-gray-800 py-1.5 px-4 text-sm font-semibold text-gray-400 shadow">
-                    Copied!
-                  </div>
-                  <Popover.Arrow className="fill-white/20" />
-                </motion.div>
-              </Popover.Content>
-            </Popover.Portal>
-          )}
-        </AnimatePresence>
-      </Popover.Root>
+        <div className="absolute inset-y-0 right-3">
+          <Popover.Root open={open} onOpenChange={handleOpenChange}>
+            <Popover.Trigger
+              asChild
+              className="hover:text-gray-400 focus:outline-none"
+            >
+              <Button>Click me</Button>
+            </Popover.Trigger>
+            <AnimatePresence>
+              {open && (
+                <Popover.Portal forceMount>
+                  <Popover.Content
+                    className=""
+                    sideOffset={5}
+                    side="top"
+                    onOpenAutoFocus={(e) => e.preventDefault()}
+                    asChild
+                  >
+                    <motion.div
+                      initial="hidden"
+                      animate="showing"
+                      exit="hidden"
+                      transition={{ duration: 0.2 }}
+                      variants={{
+                        hidden: {
+                          y: 3,
+                          opacity: 0,
+                        },
+                        showing: {
+                          y: 0,
+                          opacity: 1,
+                        },
+                      }}
+                      className="focus:outline-none"
+                    >
+                      <div className="rounded border border-white/20 bg-gray-800 py-1.5 px-4 text-sm font-semibold text-gray-400 shadow">
+                        Copied!
+                      </div>
+                      <Popover.Arrow className="fill-white/20" />
+                    </motion.div>
+                  </Popover.Content>
+                </Popover.Portal>
+              )}
+            </AnimatePresence>
+          </Popover.Root>
+        </div>
+      </div>
     </div>
   );
 }
